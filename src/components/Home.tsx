fefef0ce4240717fed2025a6d336ad8d54514152
richardhenyash <richardash1@gmail.com>
2026-02-20 07:57:20 +0800

    Test redeployment

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
             heroImage="/assets/hero.jpg"
             heroContent={
                 <p className="d-flex align-items-center m-0">
-                    Home page content lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Home page content lorem ipsum dolor sit amet, consectetur updated elit.
                     Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco.
                 </p>
